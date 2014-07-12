f2fed6104d1cbc06980e9c98b007003da790b48b
DepthDeluxe <cheinzmann3@gmail.com>
2014-07-12 10:29:33 -0700

    added codes to exceptions

--- server/src/model/FuncNotFound.js
+++ server/src/model/FuncNotFound.js
@@ -1,5 +1,6 @@
 function FuncNotFound() {
     this.name = "FuncNotFound";
+    this.code = 404;
     this.level = "Show Stopper";
     this.message = "ZeroRPC server doesn't serve this function!";
     this.toString = function(){return this.name + ": " + this.message;};
--- server/src/model/NotConnected.js
+++ server/src/model/NotConnected.js
@@ -1,6 +1,7 @@
 function NotConnected() {
     this.name = "NotConnected";
     this.level = "Show Stopper";
+    this.code = 502;
     this.message = "ZeroRPC client not connected to the server!";
     this.toString = function(){return this.name + ": " + this.message;};
 }
