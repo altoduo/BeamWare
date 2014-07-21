1049caa68cd45d6837d31adbccdf87a501959135
DepthDeluxe <cheinzmann3@gmail.com>
2014-07-20 23:12:05 -0700

    used child_process for client autoloading on a nodemon reload, the server will restart the client

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -3,10 +3,38 @@ module.exports = function(grunt) {
     grunt.initConfig({
         nodemon: {
             dev: {
-                script: 'app.js'
+                script: 'app.js',
+                options: {
+                    callback: function(nodemon) {
+                        // gets run when server first starts
+                        nodemon.on('config:update', launchPythonClient);
+
+                        // gets run when changes detected
+                        nodemon.on('restart', restartPythonClient);
+                    }
+                }
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-nodemon');
 };
+
+var child_process = require('child_process');
+var pythonProcess;
+
+function launchPythonClient() {
+    // spawn the python process
+    setTimeout(function() {
+        pythonProcess = child_process.spawn('python2', ['../SampleApp/my_app.py'], {
+            stdio: 'inherit'
+        });
+    }, 2500);
+}
+
+function restartPythonClient() {
+    // kill the current process
+    pythonProcess.kill('SIGKILL');
+
+    launchPythonClient();
+}
