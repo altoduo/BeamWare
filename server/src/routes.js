42b2ca0021f7aaeee38d2c063ada72ebd9af634a
DepthDeluxe <cheinzmann3@gmail.com>
2014-07-20 17:52:54 -0700

    debugging for arguments again didn't commit properly

--- server/src/routes.js
+++ server/src/routes.js
@@ -60,21 +60,18 @@ router.get('/:name/:function', function(req, res) {
 
     // attempt to get the client
     var client = beamServer.clients[name];
-    if (client === undefined) {
-        throw new ex.FuncNotFoundError();
-    }
-
-    console.log('-- req.query.args --');
-    console.log(req.query.args);
+    //if (client === undefined) {
+    //    throw new ex.FuncNotFoundError();
+    //}
 
     // build up the parameters
     var args = [];
     if (req.query.args !== undefined) {
         // split on comma unless escaped
-        args = req.query.args.split(/,/);
+        args = req.query.args.split(/[^\\],/);
     }
 
-    console.log('-- arg list --');
+    console.log('-- here are the args --');
     console.log(args);
 
     client.call(fn, args)
